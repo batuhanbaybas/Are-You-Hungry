6181026e13b948f4690040895d4402e47f5167d6
batuhanbaybas <71324968+batuhanbaybas@users.noreply.github.com>
2021-02-19 16:41:26 +0300

    master

--- script/index.js
+++ script/index.js
@@ -1,10 +1,8 @@
 let allCategoryApi = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
-let filterCategoryApi = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
 let category = document.querySelector("#category");
 let changeCategory = document.querySelector("#categoryList");
 let favorite = document.querySelector("#favorite");
 let filterCategory = document.querySelector("#categoryFilter");
-let filterCategoryR = document.querySelector("#categoryFilter-r")
 
 fetch(allCategoryApi)
   .then((response) => {
@@ -31,33 +29,29 @@ fetch(allCategoryApi)
         .then((e) => {
           for (let i = 0; i < e.meals.length; i++) {
             const element = e.meals[i];
-            console.log(element.strMeal,)
-            let div = document.createElement("div")
-            div.classList = "card border border-warning mt-5"
-            div.setAttribute("style","width:18rem;")
-            filterCategory.appendChild(div)
-            let image = document.createElement("img")
-            image.classList = "card-img-top"
-            image.setAttribute("src", element.strMealThumb)
-            div.appendChild(image)     
-            let newDiv = document.createElement("div")
-            newDiv.classList = "card-body"
-            div.appendChild(newDiv)
-            let h5 = document.createElement("h5")
-            h5.classList = "card-title"
-            h5.innerHTML = element.strMeal
-            newDiv.appendChild(h5)
-            let button = document.createElement("button")
-            button.classList = "btn btn-primary"
-            button.innerText = "Tarif için tıkla"
-            newDiv.appendChild(button)
-            
+            console.log(element.strMeal);
+            let div = document.createElement("div");
+            div.classList = "card border border-warning mt-5";
+            div.setAttribute("style", "width:18rem;");
+            filterCategory.appendChild(div);
+            let image = document.createElement("img");
+            image.classList = "card-img-top";
+            image.setAttribute("src", element.strMealThumb);
+            div.appendChild(image);
+            let newDiv = document.createElement("div");
+            newDiv.classList = "card-body";
+            div.appendChild(newDiv);
+            let h5 = document.createElement("h5");
+            h5.classList = "card-title";
+            h5.innerHTML = element.strMeal;
+            newDiv.appendChild(h5);
+            let button = document.createElement("button");
+            button.classList = "btn btn-primary";
+            button.innerText = "Tarif için tıkla";
+            newDiv.appendChild(button);
           }
         });
 
       console.log(value);
-      GetIngradients = (value) => {
-        console.log(value);
-      };
     };
   });
